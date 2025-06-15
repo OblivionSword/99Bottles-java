08c9ff4f681dc90f13cbb118a0c8425139544691
OblivionSword <hunter140797@gmail.com>
2025-06-15 19:21:17 +0200

    remove variable song in verses method in Chapter3_CodeSmell class

--- src/Chapter3_CodeSmell.java
+++ src/Chapter3_CodeSmell.java
@@ -2,27 +2,26 @@ public class Chapter3_CodeSmell {
 
     public String verses(int number)
     {
-        String song = "";
 
          switch (number) {
             case 0:
-                return song = "No more bottles of milk on the wall, " + 
-                              "no more bottles of milk.\n" + 
-                              "Go to the store and buy some more, " + 
-                              "99 bottles of milk on the wall.\n";
+                return  "No more bottles of milk on the wall, " + 
+                        "no more bottles of milk.\n" + 
+                        "Go to the store and buy some more, " + 
+                        "99 bottles of milk on the wall.\n";
 
             case 1:
-                return song = "1 bottle of milk on the wall, " +
-                              "1 bottle of milk.\n" +
-                              "Take it down and pass it around, " +
-                              "no more bottles of milk on the wall.\n";
+                return  "1 bottle of milk on the wall, " +
+                        "1 bottle of milk.\n" +
+                        "Take it down and pass it around, " +
+                        "no more bottles of milk on the wall.\n";
             
             default:
-                return song = number + " bottles of milk on the wall, " +
-                              number + " bottles of milk.\n" +
-                              "Take one down and pass it around, " +
-                              (number-1) + this.container(number - 1) 
-                              + " of milk on the wall.\n";
+                return  number + " bottles of milk on the wall, " +
+                        number + " bottles of milk.\n" +
+                        "Take one down and pass it around, " +
+                        (number - 1) + this.container(number - 1) 
+                        + " of milk on the wall.\n";
          }
     }
 
